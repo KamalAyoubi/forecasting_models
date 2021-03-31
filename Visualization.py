ba6a900bb924346f7fbdd1d83fbaf8581578fcad
KamalAyoubi <78489300+KamalAyoubi@users.noreply.github.com>
2021-03-31 15:40:12 +0200

    first app on 2 datasets

--- Visualization.py
+++ Visualization.py
@@ -8,15 +8,17 @@ from matplotlib import pyplot as plt
 from download import download
 from datetime import datetime
 from scipy import stats as st
-#import plotly.graph_objects as go
 import calendar
 import json
 from pandas import json_normalize
-
 from ipywidgets import interact
-#import path
-
+from pandas import Series
 # %%
+#                                             Importe Data 
+#------------------------------------------------------------------------------------------------------------------------
+
+
+#%%
 #Importe Data:
 url = 'https://data.montpellier3m.fr/sites/default/files/ressources/MMM_EcoCompt_X2H20042633_archive.json'
 path_target = "bike_traffic.json"
@@ -38,17 +40,13 @@ download(url1, path_target, replace=False)
 #%%
 #Read .Json data as dataframe#Display of the first 5 lines 
 bike_traffic_df1 = pd.read_json('bike_traffic1.json', lines=True)
-     
 
-
-
-bike_traffic_df1.head(n=2)
 
 #%%
 
-
-
-from pandas import Series
+# %%
+#                                             Preparing Data 1
+#------------------------------------------------------------------------------------------------------------------------
 
 
 #%%
@@ -60,20 +58,19 @@ data_test['start_of_day'] = data_test['start_of_day'].str.replace('T',' ')
 
 time_improved = pd.to_datetime(data_test['start_of_day'] ,
                                format='%Y-%m-%d %H:%M:%S')
-time_improved
-
-# %%                              
+time_improved                       
 
 data_test['start_day'] = time_improved
 
-
-# %%
 data_test = data_test.set_index(['start_day'])
 
+# %%
+#                                             Preparing Data 1
+#------------------------------------------------------------------------------------------------------------------------
+
 #%%
-###################teste
 bike_traffic_df=bike_traffic_df.rename(columns = {'intensity': 'latte1'})
-#%%
+
 bike_traffic_df1=bike_traffic_df1.rename(columns = {'intensity': 'latte2'}) 
 
 
@@ -83,49 +80,12 @@ d = [bike_traffic_df['latte1'], bike_traffic_df1['latte2']]
 
 data_test22 = pd.DataFrame([bike_traffic_df['latte1'], bike_traffic_df1['latte2']])
 data_test22=data_test22.T
-#%%
-data_test22['startday'] = time_improved
-#%%
-data_test22 = data_test22.set_index(['startday'])
-# %%
-#Display of the last 5 lines 
-bike_traffic_df1.tail(n=2)
 
 
 # %%
-#Checking data columns
-bike_traffic_df.columns
+#                                             visualisation
+#------------------------------------------------------------------------------------------------------------------------
 
-df_titanic.describe()
-df_titanic.info()
-
-
-#%%
-from ipywidgets import interact
-
-
-#%%
-from pandas import Series
-
-
-#%%
-data_test=bike_traffic_df2.join(bike_traffic_df2['dateObserved'].apply(lambda x: Series(x.split('/'))))
-data_test=data_test.rename(columns = {0: 'start_of_day', 1: 'end_of_day'}) 
-
-data_test['start_of_day'] = data_test['start_of_day'].str.replace('T',' ')
-#%%
-
-time_improved = pd.to_datetime(data_test['start_of_day'] ,
-                               format='%Y-%m-%d %H:%M:%S')
-time_improved
-
-# %%                              
-
-data_test['start_day'] = time_improved
-
-
-# %%
-data_test = data_test.set_index(['start_day'])
 
 #%%
 
